0076ddc2f37e63d8c9f4d9624e83cb05510e92ac
Jonathan Damico <jj_damico@yahoo.com>
2015-06-11 20:53:13 -0700

    init commit

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+
+class searchfortutor {
+  public function post {
+    $classname = $this->request->get("class");
+    $lastname = $this->request->get("lastname");
+    $school = $this->request->get("school");
+
+   
+  }
+}
